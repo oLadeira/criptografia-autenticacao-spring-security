6ae38599e29e16549969c0cad7095b20f5307e3a
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-12-23 15:56:13 -0300

    :sparkles: feature:  metodo save

--- src/main/java/com/lucasladeira/controllers/UserController.java
+++ src/main/java/com/lucasladeira/controllers/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +25,12 @@ public class UserController {
 	public ResponseEntity<List<User>> getAll(){
 		List<User> users = userRepository.findAll();
 		return ResponseEntity.ok().body(users);
+	}
+	
+	@PostMapping("/save")
+	public ResponseEntity<User> save(@RequestBody User user){
+		user = userRepository.save(user);
+		return ResponseEntity.ok().body(user);
 	}
 	
 }
