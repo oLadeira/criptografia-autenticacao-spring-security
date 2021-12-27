123178c57b41646cdfa59143ee9ae590bfe1c6d2
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-12-27 09:26:39 -0300

    :sparkles: feature:  encriptando password e validando com encoder.matches

--- src/main/java/com/lucasladeira/CriptografiaSpringSecurityApplication.java
+++ src/main/java/com/lucasladeira/CriptografiaSpringSecurityApplication.java
@@ -2,12 +2,24 @@ package com.lucasladeira;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CriptografiaSpringSecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CriptografiaSpringSecurityApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
+	}
+	
 }
--- src/main/java/com/lucasladeira/controllers/UserController.java
+++ src/main/java/com/lucasladeira/controllers/UserController.java
@@ -1,11 +1,17 @@
 package com.lucasladeira.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucasladeira.entities.User;
@@ -18,11 +24,39 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PasswordEncoder encoder;
 	
 	@GetMapping("/getAll")
 	public ResponseEntity<List<User>> getAll(){
 		List<User> users = userRepository.findAll();
 		return ResponseEntity.ok().body(users);
+	}
+	
+	@PostMapping("/save")
+	public ResponseEntity<User> save(@RequestBody User user){
+		user.setPassword(encoder.encode(user.getPassword()));
+		user = userRepository.save(user);		
+		return ResponseEntity.ok().body(user);
+	}
+	
+	@GetMapping("/validaSenha")
+	public ResponseEntity<User> validarSenha(@RequestParam String login, @RequestParam String password){
+		
+		Optional<User> opt = userRepository.findByLogin(login);
+		
+		if (opt.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}
+		
+		boolean valid = false;
+		
+		valid = encoder.matches(password, opt.get().getPassword());
+		
+		HttpStatus status = (valid) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+		
+		return ResponseEntity.status(status.value()).build();
+		
 	}
 	
 }
